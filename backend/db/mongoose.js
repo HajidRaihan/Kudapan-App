526fa674bfbb0c7660329f7fc33df63a1a02f1b8
Hajid Raihan <hajidraihan@gmail.com>
2024-08-08 22:23:25 +0800

    mau deploy di vercel bismillah bisa

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -4,6 +4,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/kudapan")
+  .connect("mongodb+srv://root:31072002@atlascluster.oh7ia.mongodb.net/kudapan_db")
   .then(() => console.log("database connected"))
   .catch((err) => console.log("database connecttion is error", err));
