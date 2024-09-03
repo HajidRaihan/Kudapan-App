24cdfa0cf54db73c62afb75fe209af3cdbbe7cd4
Hajid Raihan <hajidraihan@gmail.com>
2024-09-03 16:11:27 +0800

    change db url

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -7,6 +7,6 @@ dotenv.config();
 const mongodb = "mongodb://localhost:27017/kudapan";
 
 mongoose
-  .connect("mongodb+srv://root:31072002@atlascluster.oh7ia.mongodb.net/kudapan_db")
+  .connect("mongodb://localhost:27017/kudapan")
   .then(() => console.log("database connected"))
   .catch((err) => console.log("database connecttion is error", err));
